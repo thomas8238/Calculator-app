f617d314455888009a16ad8fa4c172bf3861c8bf
thomas8238 <90787151+thomas8238@users.noreply.github.com>
2022-08-19 08:20:21 -0500

    started calculator app

---
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mycalculatorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void findSum(View view) {
+        EditText number1ET = findViewById(R.id.num1ET);
+        EditText number2ET = findViewById(R.id.num2ET);
+        TextView numberSumTV = findViewById(R.id.resultTV);
+
+        int num1 = Integer.parseInt((number1ET.getText().toString()));
+        int num2 = Integer.parseInt((number2ET.getText().toString()));
+        int sum = num1 + num2;
+
+        numberSumTV.setText("Answer:  " + sum);
+    }
+}
