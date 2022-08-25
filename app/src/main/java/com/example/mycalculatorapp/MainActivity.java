486f9e27373e4c6316140b72a4e47a9ef06454fa
thomas8238 <90787151+thomas8238@users.noreply.github.com>
2022-08-25 08:19:57 -0500

    finished code added extras

--- app/src/main/java/com/example/mycalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorapp/MainActivity.java
@@ -25,90 +25,72 @@ public class MainActivity extends AppCompatActivity {
         number2ET.setText("");
     }
     public void findSum(View view) {
+        DecimalFormat n = new DecimalFormat("##.0000");
         EditText number1ET = findViewById(R.id.firstValue);
         EditText number2ET = findViewById(R.id.SecondValue);
-        boolean mod = false;
         TextView numberSumTV = findViewById(R.id.result);
-        if(number2ET.getText().length() == 0) {
-            number2ET.setText("0");
-            mod = true;
-        }
-        if(number1ET.getText().length() == 0) {
-            number1ET.setText("0");
-            mod = true;
+        if(number1ET.getText().length() == 0 || number2ET.getText().length() == 0) {
+            toastNoVal(view);
+            return;
         }
         try {
-            int num1 = Integer.parseInt((number1ET.getText().toString()));
-            int num2 = Integer.parseInt((number2ET.getText().toString()));
-            int sum = num1 + num2;
-            numberSumTV.setText("Answer:  " + sum);
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double sum = num1 + num2;
+            if(sum%1 ==0) {
+                numberSumTV.setText("Answer:  " + (int)sum);
+            } else {
+                numberSumTV.setText("Answer:  " + n.format(sum));
+            }
         } catch (Exception e) {
             toastExecute(view);
-        }
-        if(mod) {
-            clear(view);
         }
 
     }
     public void findProduct(View view) {
+        DecimalFormat n = new DecimalFormat("##.0000");
         EditText number1ET = findViewById(R.id.firstValue);
         EditText number2ET = findViewById(R.id.SecondValue);
         TextView numberProductTV = findViewById(R.id.result);
-        boolean mod = false;
-        if(number1ET.getText().length() == 0 && number2ET.getText().length() == 0) {
-            number1ET.setText("0");
-            number2ET.setText("0");
-            mod = true;
+        if(number1ET.getText().length() == 0 || number2ET.getText().length() == 0) {
+            toastNoVal(view);
+            return;
         }
-        if(number2ET.getText().length() == 0) {
-            number2ET.setText("1");
-            mod = true;
-        }
-        if(number1ET.getText().length() == 0) {
-            number1ET.setText("1");
-            mod = true;
-        }
-
         try {
-            int num1 = Integer.parseInt((number1ET.getText().toString()));
-            int num2 = Integer.parseInt((number2ET.getText().toString()));
-            int product = num1 * num2;
-            numberProductTV.setText("Answer:  " + product);
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double product = num1 * num2;
+            if(product%1 ==0) {
+                numberProductTV.setText("Answer:  " + (int)product);
+            } else {
+                numberProductTV.setText("Answer:  " + n.format(product));
+            }
         } catch (Exception e) {
             toastExecute(view);
         }
-        if(mod) {
-            clear(view);
-        }
-
 
     }
     public void findDifference(View view) {
+        DecimalFormat n = new DecimalFormat("##.0000");
         EditText number1ET = findViewById(R.id.firstValue);
         EditText number2ET = findViewById(R.id.SecondValue);
         TextView numberDifferenceTV = findViewById(R.id.result);
-        boolean mod = false;
-        if(number2ET.getText().length() == 0) {
-            number2ET.setText("0");
-            mod = true;
-        }
-        if(number1ET.getText().length() == 0) {
-            number1ET.setText("0");
-            mod = true;
+        if(number1ET.getText().length() == 0 || number2ET.getText().length() == 0) {
+            toastNoVal(view);
+            return;
         }
         try {
-            int num1 = Integer.parseInt((number1ET.getText().toString()));
-            int num2 = Integer.parseInt((number2ET.getText().toString()));
-            int difference = num1 - num2;
-            numberDifferenceTV.setText("Answer:  " + difference);
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double difference = num1 - num2;
+            if(difference%1 ==0) {
+                numberDifferenceTV.setText("Answer:  " + (int)difference);
+            } else {
+                numberDifferenceTV.setText("Answer:  " + n.format(difference));
+            }
         } catch (Exception e) {
             toastExecute(view);
         }
-        if(mod) {
-            clear(view);
-        }
-
-
 
     }
     public void findQuotient(View view) {
@@ -116,36 +98,30 @@ public class MainActivity extends AppCompatActivity {
         EditText number1ET = findViewById(R.id.firstValue);
         EditText number2ET = findViewById(R.id.SecondValue);
         TextView numberQuotientTV = findViewById(R.id.result);
-        boolean mod = false;
-        if(number1ET.getText().length() == 0 && number2ET.getText().length() == 0) {
-            number1ET.setText("0");
-            number2ET.setText("0");
-            mod = true;
+        if(number1ET.getText().length() == 0 || number2ET.getText().length() == 0) {
+            toastNoVal(view);
+            return;
         }
-        if(number2ET.getText().length() == 0) {
-            number2ET.setText("1");
-            mod = true;
-        }
-        if(number1ET.getText().length() == 0) {
-            number1ET.setText("1");
-            mod = true;
-        }
-
         try {
-            int num1 = Integer.parseInt((number1ET.getText().toString()));
-            int num2 = Integer.parseInt((number2ET.getText().toString()));
-            double quotient = (double) num1 / num2;
-            numberQuotientTV.setText("Answer:  " + n.format(quotient));
+
+            double num1 = Double.parseDouble((number1ET.getText().toString()));
+            double num2 = Double.parseDouble((number2ET.getText().toString()));
+            double quotient = (double) num1/num2;
+            if(quotient%1 ==0) {
+                numberQuotientTV.setText("Answer:  " + (int)quotient);
+            } else {
+                numberQuotientTV.setText("Answer:  " + n.format(quotient));
+            }
         } catch (Exception e) {
             toastExecute(view);
-        }
-        if(mod) {
-            clear(view);
-
         }
     }
     public void toastExecute(View v) {
         Toast toast = Toast.makeText(getApplicationContext(), "Numbers are too large", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+    public void toastNoVal(View v) {
+        Toast toast = Toast.makeText(getApplicationContext(), "Enter a value into each text box", Toast.LENGTH_SHORT);
         toast.show();
     }
 }
